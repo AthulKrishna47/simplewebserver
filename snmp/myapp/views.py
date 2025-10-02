7cb8d823cc60a23cb0f2f03bf9a7ed2521821da9
AthulKrishna47 <athullkrishnaav171@gmail.com>
2025-10-02 19:44:25 +0530

    Success

--- snmp/myapp/views.py
+++ snmp/myapp/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponrenderse
+
+def home(request):
+    return render(request, 'home.html')
 
 # Create your views here.
--- web.py
+++ web.py
@@ -1,42 +1,3 @@
-# EX01 Developing a Simple Webserver
-## Date: 02/10/2025
-
-## AIM:
-To develop a simple webserver to serve html pages and display the Device Specifications of your Laptop.
-
-## DESIGN STEPS:
-### Step 1: 
-HTML content creation.
-
-### Step 2:
-Design of webserver workflow.
-
-### Step 3:
-Implementation using Python code.
-
-### Step 4:
-Import the necessary modules.
-
-### Step 5:
-Define a custom request handler.
-
-### Step 6:
-Start an HTTP server on a specific port.
-
-### Step 7:
-Run the Python script to serve web pages.
-
-### Step 8:
-Serve the HTML pages.
-
-### Step 9:
-Start the server script and check for errors.
-
-### Step 10:
-Open a browser and navigate to http://127.0.0.1:8000 (or the assigned port).
-
-## PROGRAM:
-'''
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
 HOST = 'localhost'
@@ -83,10 +44,3 @@ if __name__ == '__main__':
     except KeyboardInterrupt:
         print("\nShutting down server.")
         server.server_close()
-'''
-
-## OUTPUT:
-![alt text](<Screenshot 2025-10-02 193800.png>)    
-![alt text](<Screenshot 2025-10-02 193739.png>)
-## RESULT:
-The program for implementing simple webserver is executed successfully.
